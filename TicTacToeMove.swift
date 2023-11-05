68b8dd15b9555ab84061c2b2cb8e0d11d8deb462
Thomas Cavalli <82778944+UNIVAC-Coder@users.noreply.github.com>
2023-11-05 15:32:36 -0500

    Create TicTacToeMove.swift
    The class defining a tic-tac-toe move.

---
+++ TicTacToeMove.swift
@@ -0,0 +1,54 @@
+//
+//  TicTacToeMove.swift
+//  Snapshot Tic-Tac-Toe
+//  Creative Commons Attribution 4.0 International Public License
+//  Creative Commons may be contacted at creativecommons.org.
+//
+//  Copyright (c) 2021 Thomas Cavalli
+//  Thomas Cavalli may be contacted at thomascavalli.com
+//
+//  List of What Changed (by Who and When):
+//  Created by Thomas Cavalli on 11/4/21.
+//  Complete reWrite to SwiftUI on IPad using Swift Playgrounds on 11/4/23.
+//
+
+import Foundation
+
+class TicTacToeMove: Identifiable, Codable {
+    
+    var id                  :UUID = UUID()
+    var board               :[Int] = [11,11,11,11,11,11,11,11,11, // 1 is green boarder, 3 is X, 9 is O, 11 is blank
+                                      11,11,11,11,11,11,11,11,11, // 90 degrees
+                                      11,11,11,11,11,11,11,11,11, // 180 degrees
+                                      11,11,11,11,11,11,11,11,11] // 270 degrees
+    var comment             :String = "X moves first."
+    var index               :Int = 0
+    var isSelected          :Bool = true
+    
+    init() {
+        self.id = UUID()
+        self.board    = [11,11,11,11,11,11,11,11,11,
+                         11,11,11,11,11,11,11,11,11,
+                         11,11,11,11,11,11,11,11,11,
+                         11,11,11,11,11,11,11,11,11]
+        self.comment = "X moves first."
+        self.index = 0
+        self.isSelected = true
+    }
+    
+    init(id: UUID, board: [Int], comment: String, index: Int, selected: Bool) {
+        self.id = id
+        self.board    = board
+        self.comment = comment
+        self.index = index
+        self.isSelected = selected
+    }
+    
+    init(copyBoard: TicTacToeMove) {
+        self.id = UUID()
+        self.board    = Array(copyBoard.board)
+        self.comment = String(copyBoard.comment)
+        self.index = Int(copyBoard.index)
+        self.isSelected = Bool(copyBoard.isSelected)
+    }
+}
