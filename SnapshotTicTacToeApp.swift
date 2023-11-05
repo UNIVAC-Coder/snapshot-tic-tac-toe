998a3fb95f63a15ad2df09915746f524f174ee9e
Thomas Cavalli <82778944+UNIVAC-Coder@users.noreply.github.com>
2023-11-05 14:40:21 -0500

    Create SnapshotTicTacToeApp.swift
    The main App in Swift Playground.

---
+++ SnapshotTicTacToeApp.swift
@@ -0,0 +1,25 @@
+//
+//  SnapshotTicTacToeApp.swift
+//  Snapshot Tic-Tac-Toe
+//  Creative Commons Attribution 4.0 International Public License
+//  Creative Commons may be contacted at creativecommons.org.
+//
+//  Copyright (c) 2021 Thomas Cavalli
+//  Thomas Cavalli may be contacted at thomascavalli.com
+//
+//  List of What Changed (by Who and When):
+//  Created by Thomas Cavalli on 11/4/21.
+//  Complete reWrite to SwiftUI on IPad using Swift Playgrounds on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotTicTacToeApp: App {
+         
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
